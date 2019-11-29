b5be74549d8ecc1363da580f3d645f6dddd9fd1e
Dwayne Bratcher <dbratcher@wvsd.local>
2019-11-28 22:30:56 -0600

    initializing dotnet-dog-spa repository

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_dog_spa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
